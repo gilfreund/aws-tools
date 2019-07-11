6f96930cf2456995f992cc18a0eb2fe0847a89c6
Gil Freund <gilf@sysnet.co.il>
2019-07-11 17:41:07 +0300

    Update awsSGupdate.sh

--- awsSGupdate/awsSGupdate.sh
+++ awsSGupdate/awsSGupdate.sh
@@ -16,14 +16,14 @@ NEWIP="$(curl -s v4.ifconfig.co)/32"
 USER="$(aws iam --output text get-user| awk '{printf $7}')"
 DESCRIPTION="user:$USER"
 
-# Part 1: get a lisy of all SG matching the Description
+# Part 1: get a list of all SG matching the Description
 SECURITYGROUPS=$($AWSCMD describe-security-groups  --query "SecurityGroups[?IpPermissions[?IpRanges[?Description=='$DESCRIPTION']]].[GroupId]")
 if [ -z "$SECURITYGROUPS" ]
 then
         echo "No Security Groups found based on the DESCRIPTION $DESCRIPTION"
         exit 1
 fi
-# Part 2: Create array from the list of groups and get a list of Permiisions (IP and Ports)
+# Part 2: Create array from the list of groups and get a list of ortocols IP and Portsx)
 IFS=$'\n' read -r -a SECURITYGROUP -d '' <<< "$SECURITYGROUPS"
 # For each security group
 for i in "${SECURITYGROUP[@]}"
@@ -31,23 +31,30 @@ do
         SECURITYGROUPID=$i
         echo Security Group $SECURITYGROUP
         echo ================================
-        echo -e "Old IP\t\t\tProt\tFrom\tTo\t"
-        IPPERMISSIONS=$($AWSCMD describe-security-groups  --group-id $i --query "SecurityGroups[?IpPermissions[?IpRanges[?Description=='$DESCRIPTION']]].IpPermissions[*].{a:IpRanges[0].CidrIp,b:IpProtocol,c:FromPort,d:ToPort}")
-# Part 3
-        IFS=$'\n' read -r -a IPPERMISSION -d '' <<< "$IPPERMISSIONS"
-        for j in "${IPPERMISSION[@]}"
+        PROTOCOLS=$($AWSCMD describe-security-groups --group-id $SECURITYGROUPID --query "SecurityGroups[?IpPermissions[?IpRanges[?Description=='$DESCRIPTION']]].IpPermissions[*].{a:IpProtocol,b:FromPort,c:ToPort}")
+        IFS=$'\n' read -r -a PROTOCOLARRAY -d '' <<< "$PROTOCOLS"
+        for p in "${PROTOCOLARRAY[@]}"
         do
-                OLDIP=$(echo "$j" | awk '{print $1}')
-                PROTOCOL=$(echo "$j" | awk '{print $2}')
-                FROMPORT=$(echo "$j" | awk '{print $3}')
-                TOPORT=$(echo "$j" | awk '{print $4}')
-                echo -e "$OLDIP\t$PROTOCOL\t$FROMPORT\t$TOPORT"
-                $AWSCMD revoke-security-group-ingress \
-                --group-id $SECURITYGROUPID \
-                --ip-permissions IpProtocol=$PROTOCOL,FromPort=$FROMPORT,ToPort=$TOPORT,IpRanges="[{CidrIp=$OLDIP}]"
+                echo protocol and port $p
+                echo ----------------------------------
+                echo -e "Old IP\t\t\tProt\tFrom\tTo\t"
+                PROTOCOL=$(echo "$p" | awk '{print $1}')
+                FROMPORT=$(echo "$p" | awk '{print $2}')
+                TOPORT=$(echo "$p" | awk '{print $3}')
+                OLDIPS=$($AWSCMD describe-security-groups --group-id $SECURITYGROUPID  --query "SecurityGroups[?IpPermissions[?IpRanges[*]]].IpPermissions[*].{a:IpRanges[?Description=='$DESCRIPTION'].CidrIp}")
+# Part 3 - Get all IPs
+                IFS=$'\n' read -r -a OLDIPARRAY -d '' <<< "$OLDIPS"
+                for j in "${OLDIPARRAY[@]}"
+                do
+                        OLDIP=$(echo "$j" | awk '{printf $2}')
+                        echo -e "$OLDIP\t$PROTOCOL\t$FROMPORT\t$TOPORT"
+#               $AWSCMD revoke-security-group-ingress \
+#               --group-id $SECURITYGROUPID \
+#               --ip-permissions IpProtocol=$PROTOCOL,FromPort=$FROMPORT,ToPort=$TOPORT,IpRanges="[{CidrIp=$OLDIP}]"
 
-                $AWSCMD authorize-security-group-ingress \
-                --group-id $SECURITYGROUPID \
-                --ip-permissions IpProtocol=$PROTOCOL,FromPort=$FROMPORT,ToPort=$TOPORT,IpRanges="[{CidrIp=$NEWIP,Description=$DESCRIPTION}]"
+#               $AWSCMD authorize-security-group-ingress \
+#               --group-id $SECURITYGROUPID \
+#               --ip-permissions IpProtocol=$PROTOCOL,FromPort=$FROMPORT,ToPort=$TOPORT,IpRanges="[{CidrIp=$NEWIP,Description=$DESCRIPTION}]"
+                done
         done
 done
